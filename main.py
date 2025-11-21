4f64e8faf4637137e3f9a5d8b1ea11b9e40e9df5
Pablo Medina <113461293+ppablomedina@users.noreply.github.com>
2025-11-21 11:21:22 +0000

    Implement driver setup for Nextbike downloads
    Added driver setup and download directory management for Nextbike data extraction.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask          import Flask, request
-from nextbike.utils import download_from_nextbike, log_in_nextbike
+from nextbike.utils import set_driver, download_from_nextbike, log_in_nextbike
 from nextbike.paths import *
 from gcp.utils      import upload_to_gcp
 from gcp.paths      import *
@@ -11,30 +11,42 @@ def entry_point(req):
 
     dic = {}
 
-    log_in_nextbike(link_login)
+    # Crear driver y carpeta de descargas SOLO para esta ejecución
+    driver, download_dir = set_driver()
 
-    # dic[path_abonos]                   = download_from_nextbike(link_abonos)
+    try:
+        # 1) Login en Nextbike
+        log_in_nextbike(driver, link_login)
 
-    # dic[path_vehiculos_anclados]       = download_from_nextbike(link_vehiculos_anclados)
-    # dic[path_vehiculos_coords]         = download_from_nextbike(link_vehiculos_coords)
+        # 2) Descargas que quieras hacer en esta ejecución
+        # dic[path_abonos]                   = download_from_nextbike(driver, download_dir, link_abonos)
 
-    # dic[path_clientes_registrados]     = download_from_nextbike(link_clientes_registrados)
-    # dic[path_clientes_detalles]        = download_from_nextbike(link_clientes_detalles)
-    dic[path_clientes_ultimo_alquiler] = download_from_nextbike(link_clientes_ultimo_alquiler)
+        # dic[path_vehiculos_anclados]       = download_from_nextbike(driver, download_dir, link_vehiculos_anclados)
+        # dic[path_vehiculos_coords]         = download_from_nextbike(driver, download_dir, link_vehiculos_coords)
 
-    # dic[path_alquileres]               = download_from_nextbike(link_alquileres)
-    # dic[path_alquileres_con_abono]     = download_from_nextbike(link_alquileres_con_abono)
-    # dic[path_alquileres_sin_abono]     = download_from_nextbike(link_alquileres_sin_abono)
+        # dic[path_clientes_registrados]     = download_from_nextbike(driver, download_dir, link_clientes_registrados)
+        # dic[path_clientes_detalles]        = download_from_nextbike(driver, download_dir, link_clientes_detalles)
+        dic[path_clientes_ultimo_alquiler] = download_from_nextbike(driver, download_dir, link_clientes_ultimo_alquiler)
 
-    # dic[path_revisiones] = download_from_gs_excel(link_revisiones)
-    # dic[path_inventario] = download_from_gs_single_sheet(link_inventario)
-    # dic[path_repuestos]  = download_from_gs_excel(link_repuestos)
+        # dic[path_alquileres]               = download_from_nextbike(driver, download_dir, link_alquileres)
+        # dic[path_alquileres_con_abono]     = download_from_nextbike(driver, download_dir, link_alquileres_con_abono)
+        # dic[path_alquileres_sin_abono]     = download_from_nextbike(driver, download_dir, link_alquileres_sin_abono)
 
-    # ! Me falta incidencias y recaudación
+        # dic[path_revisiones] = download_from_gs_excel(link_revisiones)
+        # dic[path_inventario] = download_from_gs_single_sheet(link_inventario)
+        # dic[path_repuestos]  = download_from_gs_excel(link_repuestos)
 
-    upload_to_gcp(dic)
+        # ¡Faltan incidencias y recaudación cuando los tengas!
 
-    return "ETL ejecutado correctamente\n", 200
+        # 3) Subir todo a GCP
+        upload_to_gcp(dic)
+
+        return "ETL ejecutado correctamente\n", 200
+
+    finally:
+        # Cerrar siempre el navegador para no dejar procesos colgados
+        driver.quit()
+
 
 app = Flask(__name__)
 
